d71dab0f67381eb33454f11e70035b6a64e15ef2
JeanTowers <jean.carlos.torres@hotmail.com>
2023-03-29 18:22:54 -0400

    Aprimorando a aplicação - TypeScript no React

---
+++ 01-fundamentos-reactjs-ts/src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import styles from  './Sidebar.module.css'
+import { PencilLine } from "phosphor-react";
+import { Avatar } from './Avatar';
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                className={styles.cover}
+                src='https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+            />
+            
+            <div className={styles.profile}>
+                <Avatar src="https://github.com/JeanTowers.png"/>
+                <strong>Jean Torres</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
