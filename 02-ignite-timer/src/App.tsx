b656b1bc53af4e203a234c93a6dea100d9845106
JeanTowers <jean.carlos.torres@hotmail.com>
2023-04-05 11:51:32 -0400

    Estrutura da aplicação - Configurando ESLint

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
 import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
-import { defaultTHeme } from './styles/themes/default'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTHeme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="danger"/>
-      <Button variant="success"/>
-      <Button/>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
-
-
