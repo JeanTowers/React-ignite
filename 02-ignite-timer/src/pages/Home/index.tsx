1cfbe2907e600a900b784e7547dc83f1654af92e
JeanTowers <jean.carlos.torres@hotmail.com>
2023-04-07 13:53:02 -0400

    Páginas e rotas - Finalização da Home

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -3,19 +3,27 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountINput,
   Separator,
+  StartCountdownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
   return (
     <HomeContainer>
-      <form action="">
+      <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" placeholder="Dê um nome para seu projeto" />
 
-          <label htmlFor="minutesAmount">Vou trabalhar em</label>
-          <input type="number" id="minutesAmount" />
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountINput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
+
           <span>minutos.</span>
         </FormContainer>
 
@@ -27,10 +35,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton disabled type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
